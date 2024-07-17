95ef7f472ac9cdb6b35fe660dd49e8cbca1bb69d
account931 <account931@ukr.net>
2024-07-17 10:44:46 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
+
+
+Route::get('/', function () {
+    //return DB::connection()->getDatabaseName();
+    //return 'running OK';
+    return User::count();
+    //return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
